b6fb8c2b0998390ea5768e33ee92174b099f1d50
Kswear94 <Kswearingen94@yahoo.com>
2015-11-05 22:38:45 +0000

    Exercise 5

---
+++ ex5.rb
@@ -0,0 +1,17 @@
+my_name = 'Kole L. Swearingen'
+my_age = 21 # not a lie in 2015
+my_height = 68 # inches
+my_weight = 165 # lbs
+my_eyes = 'Brown'
+my_teeth = 'White'
+my_hair = 'Black'
+
+puts "Let's talk about #{my_name}."
+puts "I'm #{my_height} inches tall."
+puts "I'm #{my_weight} pounds heavy."
+puts "Actually that's not to heavy."
+puts "I have #{my_eyes} eyes and #{my_hair} hair."
+puts "My teeth are usually #{my_teeth} depending on the coffee."
+
+# This line is tricky, try to get it exactly right
+puts "If I add #{my_age}, #{my_height}, and #{my_weight} I get #{my_age + my_height + my_weight}."
