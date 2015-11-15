a2ba0328efb6f4ec59eec0a32823f24cbe9cf95e
Kswear94 <Kswearingen94@yahoo.com>
2015-11-15 20:55:05 +0000

    Exercise 22

---
+++ ex22.rb
@@ -0,0 +1,7 @@
+puts "puts displays content to user"
+# hashtag, pound sign. Used for comments
+puts " \+ Addition, \- Subtraction, \* multiplication, \/ division, \= Equals."
+puts "\% Percent, \< Less-than, \> Greater-than, \<= Less-than-or-equal, \>= Greater-than-or-equal."
+puts "\\n New line character, \\ Escape special chars, print prints a line without an \\n"
+puts "gets.stuff gets input from the user, .chomp cuts off the \\n, .to_i converts things to integer."
+puts "ARGV variable that holds arguments, "
