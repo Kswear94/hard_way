15e6c0e735b8313a77cbe329670b418b0ab2847a
Kswear94 <Kswearingen94@yahoo.com>
2015-12-09 23:37:16 +0000

    Exercise 34

---
+++ ex34.rb
@@ -0,0 +1,11 @@
+animals = ['bear', 'ruby', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+
+puts "The animal at 1 is ruby: #{'ruby' == animals[1]}"
+puts "The 3rd animal is at 2 and is a peacock: #{'peacock' == animals[2]}"
+puts "The 1st animal is at 0 and is a bear: #{'bear' == animals[0]}"
+puts "The animal at 3 is a kangaroo: #{'kangaroo' == animals[3]}"
+puts "The 5th animal is at 4 and is a whale: #{'whale' == animals[4]}"
+puts "The animal at 2 is a peacock: #{'peacock' == animals[2]}"
+puts "The 6th animal is at 5 and is a platypus: #{'platypus' == animals[5]}"
+puts "The animal at 4 is a whale: #{'whale' == animals[4]}"
